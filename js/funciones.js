996595c0a58116f4cf537c32b421b936114ceccc
Alexander Monzon <alexandermm2011@gmail.com>
2017-11-19 00:45:02 -0600

    Se termina la descripcion (ESTATICA) de los pokemones con html, css y funciones

--- js/funciones.js
+++ js/funciones.js
@@ -1,18 +1,22 @@
 var estado= false;
 function Select_favorit(){
   if (estado==false) {
-    console.log("cargoif");
     document.getElementById("corazon").setAttribute("src", "img/favoritoMas.png");
     document.getElementById("favor").setAttribute("src", "img/pokebola.png");
-    console.log("cargo");
     document.getElementById("corazon").style.display="block";
-    console.log("cambio block");
     estado=true;
+    setTimeout(function(){
+    document.getElementById("corazon").style.display="none";
+  },700);
 
   }
   else {
     document.getElementById("favor").setAttribute("src", "img/poke.png");
     document.getElementById("corazon").setAttribute("src", "img/favoritoMenos.png");
+    document.getElementById("corazon").style.display="block";
     estado=false;
+    setTimeout(function(){
+    document.getElementById("corazon").style.display="none";
+  },700);
   }
 }
