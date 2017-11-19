a51eb3deba306cb1b6677f1dcc321ba74bf2a03d
katiro <katirox9@gmail.com>
2017-11-19 02:36:59 -0600

    Soldado al html

--- js/prueba.js
+++ js/prueba.js
@@ -14,79 +14,92 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/"+pokemon+"/" , true);
 xmlhttp.send();
 }
-//mostrar los pokemon 
+//mostrar los pokemon
 function mostrarPokemon(){
-  document.getElementById("evolucion4").innerHTML = "";
-  document.getElementById("evolucion1").innerHTML = "";
-  document.getElementById("evolucion2").innerHTML = "";
-  document.getElementById("evolucion3").innerHTML = "";
-document.getElementById("nombre").value = pokemons.species.name;
+  //document.getElementById("evolucion4").innerHTML = "";
+  //document.getElementById("evolucion1").innerHTML = "";
+  //document.getElementById("evolucion2").innerHTML = "";
+  //document.getElementById("evolucion3").innerHTML = "";
+document.getElementById("identificador").innerHTML = "0"+pokemons.id;
+document.getElementById("nombre2").innerHTML = "Nombre: "+pokemons.species.name;
+var peso = pokemons.weight;
+peso = (peso/10);
+document.getElementById("load").innerHTML = "Peso: "+peso+" kg";
+var altura = pokemons.height;
+altura = (altura/10);
+document.getElementById("altitude").innerHTML = "Altura: "+altura+" m";
 
-document.getElementById("habilidad").value = pokemons.abilities[0].ability.name;
+document.getElementById("capacitys").value = pokemons.abilities[0].ability.name;
 
 var mov="";
-document.getElementById("movimiento").value = "";
+document.getElementById("powers").value = "";
 for(i=0 ; i<pokemons.moves.length ; i++){
   mov = mov + pokemons.moves[i].move.name+"\n";
 }
-document.getElementById("movimiento").value = mov;
+document.getElementById("powers").value = mov;
 
-document.getElementById("img").src = pokemons.sprites.front_default;
+document.getElementById("imagenpoke").src = pokemons.sprites.front_default;
 if(species.habitat != null && species.habitat != undefined){
-document.getElementById("localizacion").value = species.habitat.name;
+document.getElementById("location").innerHTML ="Ubicacion: " + species.habitat.name;
 }else {
-document.getElementById("localizacion").value = "Desconocido";
+document.getElementById("location").innerHTML = "Ubicacion: Desconocido";
 }
-var tipos="";
+document.getElementById("type1").innerHTML = "";
+document.getElementById("type2").innerHTML = "";
 for(i=0 ; i<pokemons.types.length ; i++){
-  tipos= tipos +","+ pokemons.types[i].type.name;
+  if (i == 1) {
+    document.getElementById("tipo2").style.display = "block";
+    document.getElementById("type2").innerHTML = pokemons.types[i].type.name;
+  }else {
+    document.getElementById("type1").innerHTML = pokemons.types[i].type.name;
+  }
 }
-document.getElementById("tipos").value = tipos;
+//document.getElementById("tipos").value = tipos;
 
-document.getElementById("generacion").value = species.generation.name;
+document.getElementById("generacion2").innerHTML = "Generacion: "+species.generation.name;
 for(var i=0; i<species.flavor_text_entries.length ; i++){
   if(species.flavor_text_entries[i].language.name == "es"){
-document.getElementById("descripcion").value = species.flavor_text_entries[i].flavor_text;
+document.getElementById("description").value = species.flavor_text_entries[i].flavor_text;
 break;
   }
 }
 
 //evolucion 1
-    document.getElementById("evolucion1").innerHTML = evolucion.chain.species.name;
-    aEvoluciones[0] = evolucion.chain.species.url.slice(42, -1);
-    consumidor4(aEvoluciones[iContador]);
-    iContador = 1;
-    console.log("urlEvolucion 1:  " + aEvoluciones[0]);
+    //document.getElementById("evolucion1").innerHTML = evolucion.chain.species.name;
+    //aEvoluciones[0] = evolucion.chain.species.url.slice(42, -1);
+    //consumidor4(aEvoluciones[iContador]);
+    //iContador = 1;
+    //console.log("urlEvolucion 1:  " + aEvoluciones[0]);
 
 
 //evolucion 2
-    if (evolucion.chain.evolves_to[0] != null && evolucion.chain.evolves_to[0] != undefined) {
-        for (var i = 0; i < evolucion.chain.evolves_to.length; i++) {
-            document.getElementById("evolucion2").innerHTML += evolucion.chain.evolves_to[i].species.name + "<br>";
-            aEvoluciones[iContador] = evolucion.chain.evolves_to[i].species.url.slice(42, -1);
-            console.log("urlEvolucion " + iContador + ":  " + aEvoluciones[iContador]);
-            consumidor4(aEvoluciones[iContador]);
-            iContador = iContador + 1;
-        }
+    //if (evolucion.chain.evolves_to[0] != null && evolucion.chain.evolves_to[0] != undefined) {
+        //for (var i = 0; i < evolucion.chain.evolves_to.length; i++) {
+          //  document.getElementById("evolucion2").innerHTML += evolucion.chain.evolves_to[i].species.name + "<br>";
+          //  aEvoluciones[iContador] = evolucion.chain.evolves_to[i].species.url.slice(42, -1);
+          //  console.log("urlEvolucion " + iContador + ":  " + aEvoluciones[iContador]);
+          //  consumidor4(aEvoluciones[iContador]);
+          //  iContador = iContador + 1;
+        //}
 
-    } else {
-        document.getElementById("evolucion2").innerHTML = "Desconocido";
-    }
+    //} else {
+      //  document.getElementById("evolucion2").innerHTML = "Desconocido";
+    //}
 
 //evolucion 3
-    if ((evolucion.chain.evolves_to[0] != null && evolucion.chain.evolves_to[0] != undefined) && (evolucion.chain.evolves_to[0].evolves_to[0] != null && evolucion.chain.evolves_to[0].evolves_to[0] != undefined)) {
-        for (var i = 0; i < evolucion.chain.evolves_to.length; i++) {
-            for (var j = 0; j < evolucion.chain.evolves_to[i].evolves_to.length; j++) {
-                document.getElementById("evolucion3").innerHTML += evolucion.chain.evolves_to[i].evolves_to[j].species.name+ "<br>";
-                aEvoluciones[iContador] = evolucion.chain.evolves_to[i].evolves_to[j].species.url.slice(42, -1);
-                console.log("urlEvolucion " + iContador + ":  " + aEvoluciones[iContador]);
-                consumidor4(aEvoluciones[iContador]);
-                iContador = iContador + 1;
-            }
-        }
-    } else {
-        document.getElementById("evolucion3").innerHTML = "Desconocido";
-    }
+    //if ((evolucion.chain.evolves_to[0] != null && evolucion.chain.evolves_to[0] != undefined) && (evolucion.chain.evolves_to[0].evolves_to[0] != null && evolucion.chain.evolves_to[0].evolves_to[0] != undefined)) {
+    //    for (var i = 0; i < evolucion.chain.evolves_to.length; i++) {
+      //      for (var j = 0; j < evolucion.chain.evolves_to[i].evolves_to.length; j++) {
+        //        document.getElementById("evolucion3").innerHTML += evolucion.chain.evolves_to[i].evolves_to[j].species.name+ "<br>";
+          //      aEvoluciones[iContador] = evolucion.chain.evolves_to[i].evolves_to[j].species.url.slice(42, -1);
+            //    console.log("urlEvolucion " + iContador + ":  " + aEvoluciones[iContador]);
+            //    consumidor4(aEvoluciones[iContador]);
+            //    iContador = iContador + 1;
+            //}
+        //}
+    //} else {
+      //  document.getElementById("evolucion3").innerHTML = "Desconocido";
+    //}
 
 }
 //llamar las especies
